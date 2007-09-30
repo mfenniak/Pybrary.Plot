b583b2516dc89be9b0b20c6c5a18ce2abfc52de8
mfenniak@stewart.fenniak.net <mfenniak@stewart.fenniak.net>
2007-09-30 09:32:12 -0600

    Different, better approach to fixing DateTimeAxis problem.  We're at the end of the month, we need to represent the end of the day...

--- Pybrary.Plot/DateTimeAxis.cs
+++ Pybrary.Plot/DateTimeAxis.cs
@@ -276,7 +276,7 @@ namespace Pybrary.Plot
                 {
                     DateTime maxX = asDateTime(parent.Series.MaxX.Value);
                     // scale to the end of the month of maxX;
-                    return new DateTime(maxX.Year, maxX.Month, cal.GetDaysInMonth(maxX.Year, maxX.Month) + 1);
+                    return new DateTime(maxX.Year, maxX.Month, cal.GetDaysInMonth(maxX.Year, maxX.Month), 23, 59, 59);
                 }
                 return unscaledMaximum;
             }
